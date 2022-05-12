5462304f5297cdb4f706f2a044a509a0e6bbb8c3
PoH4OLK <PoH4OO@yandex.ru>
2022-05-12 13:54:55 +0300

    Lab2

--- main.py
+++ main.py
@@ -31,3 +31,33 @@ elif discriminant == 0:
    print("x = %.2f" % x)
 else:
    print("Корней нет")
+
+   import requests
+
+   # lab2
+   s_city = "Moscow,RU"
+   appid = "6c8a83882e11461a472cf982fcf77daa"
+   res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+                      params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+   data = res.json()
+   print("\n\nГород:", s_city)
+   print("Погодные условия:", data['weather'][0]['description'])
+   print("Температура:", data['main']['temp'])
+   print("Минимальная температура:", data['main']['temp_min'])
+   print("Максимальная температура:", data['main']['temp_max'])
+   print("Максимальная температура:", data['main']['temp_max'])
+   # HomeWork wind.speed & visibility
+   print("Скорость ветра:", data['wind']['speed'])
+   print("Видимость:", data['visibility'])
+   res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+                      params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+   data = res.json()
+   print("\n\nПрогноз погоды на неделю:\n")
+   for i in data['list']:
+       print("Дата <", i['dt_txt'], "> \r\nТемпература <",
+             '{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <",
+             i['weather'][0]['description'], ">",
+             # HomeWork wind.speed & visibility
+             "\nСкорость ветра:", i['wind']['speed'],
+             "\nВидимость:", i['visibility'])
+       print("____________________________")
